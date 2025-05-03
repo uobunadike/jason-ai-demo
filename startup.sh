12e7f37fc9b400a067d7599e5594f8a8c104267f
Ugochukwu Obunadike <uobunadike@riministreet.com>
2025-05-03 10:22:05 -0500

    updates on startup.sh

--- startup.sh
+++ startup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-echo "📦 Installing dependencies..."
+echo "📦 Installing Python dependencies..."
 pip install -r requirements.txt
-
-echo "⬇️ Downloading FAISS + data from Blob..."
+echo "☁️ Downloading FAISS + data from Azure Blob..."
 python download.py
-
 echo "🚀 Launching FastAPI app with Gunicorn..."
-gunicorn api:app --bind=0.0.0.0:8000 --timeout 900
+gunicorn api:app \
+ --workers 1 \
+ --worker-class uvicorn.workers.UvicornWorker \
+ --bind 0.0.0.0:8000 \
+ --timeout 600
